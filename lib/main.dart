b11815b9bf5779fbd983820a628c12792c64a79b
kshn379 <koorain@naver.com>
2024-08-14 15:57:29 +0900

    [Feat] 1. main.dart - Flutter 외부 라이브러리 사용 2. pubspec.yaml - 'english words' dependencies 추가 3. pubspec.lock - 외부 라이브러리

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:ffi';
 
+import 'package:english_words/english_words.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -9,7 +10,7 @@ void main() {
       home: Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.deepPurpleAccent.shade100,
-          title: const Text('Flutter의 CallBack'),
+          title: const Text('Flutter에서 외부 라이브러리 사용'),
         ),
         body: const Body(),
       ),
@@ -22,65 +23,27 @@ class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const TestWidget();
+    return RandomWords();
   }
 }
 
-class TestWidget extends StatefulWidget {
-  const TestWidget({super.key});
-
-  @override
-  State<TestWidget> createState() => _TestWidgetState();
-}
-
-class _TestWidgetState extends State<TestWidget> {
-  int value = 0;
+class RandomWords extends StatelessWidget {
+  const RandomWords({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Text(
-          'Count : $value',
-          style: const TextStyle(fontSize: 30),
-        ),
-        TestButton(addCounter),
-      ],
-    );
-  }
-
-  // void addCounter() => setState(() => ++value);
-  void addCounter(int addValue) => setState(() => value = addValue + value);
-}
-
-class TestButton extends StatelessWidget {
-  const TestButton(this.callback, {super.key});
-
-  // final VoidCallback callback;
-  final Function(int) callback;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.symmetric(vertical: 8),
-      width: double.infinity,
-      child: InkWell(
-        // onTap: () => callback.call(),
-        onTap: () => callback.call(1),
-        onDoubleTap: () => callback.call(5),
-        onLongPress: () => callback.call(10),
-        child: Center(
-          child: Container(
-            padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
-            decoration: BoxDecoration(border: Border.all()),
-            child: const Text(
-              'Up Counter',
-              style: TextStyle(fontSize: 24),
-            ),
+    final wordList = generateWordPairs().take(5).toList();
+    final widgets = wordList
+        .map(
+          (word) => Text(
+            word.asCamelCase,
+            style: const TextStyle(fontSize: 32),
           ),
-        ),
-      ),
+        )
+        .toList();
+
+    return Column(
+      children: widgets,
     );
   }
 }
